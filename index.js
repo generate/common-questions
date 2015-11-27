377a76ec0469dd2bf758d572215c362bdb0ce09c
jonschlinkert <jon.schlinkert@sellside.com>
2015-11-27 14:35:49 -0500

    first commit.

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * common-questions <https://github.com/jonschlinkert/common-questions>
+ *
+ * Copyright (c) 2015, Jon Schlinkert.
+ * Licensed under the MIT License.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
