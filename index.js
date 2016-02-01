ec665c8ab5f1e34505b82c06e47355ff2c200b79
jonschlinkert <jon.schlinkert@sellside.com>
2016-02-01 16:57:10 -0500

    improve cwd logic, adds `twitter` and `email` questions

--- index.js
+++ index.js
@@ -11,21 +11,26 @@ var project = require('project-name');
 
 module.exports = function(options) {
   options = options || {};
+  var cwd = options.cwd || process.cwd();
+  var name = options.name || project(cwd);
+
   return {
     setDefault: {
       'author.name': 'Author\'s name?',
       'author.url': 'Author\'s URL?',
-      'author.username': 'Author\'s GitHub username?'
+      'author.username': 'Author\'s GitHub username?',
+      'author.twitter': 'Author\'s Twitter username?',
+      'author.email': 'Author\'s email address?',
     },
     set: {
-      name: {
+      'project.name': {
         message: 'Project name?',
-        default: project(process.cwd()),
-        force: !!options.init
+        default: name,
+        force: options.init === true
       },
-      description: {
+      'project.description': {
         message: 'Project description?',
-        force: !!options.init
+        force: options.init === true
       }
     }
   };
