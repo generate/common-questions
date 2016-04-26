463aaccf895c753baff8f6a13e93c3c17bc91e1c
jonschlinkert <jon.schlinkert@sellside.com>
2016-04-25 20:10:24 -0400

    refactor to be a base plugin

--- index.js
+++ index.js
@@ -7,31 +7,132 @@
 
 'use strict';
 
-var project = require('project-name');
+module.exports = function(config) {
+  var merge = require('mixin-deep');
 
-module.exports = function(options) {
-  options = options || {};
-  var cwd = options.cwd || process.cwd();
-  var name = options.name || project(cwd);
+  return function(app, base) {
+    if (!app.isApp || app.isRegistered('common-questions')) return;
 
-  return {
-    setDefault: {
-      'author.name': 'Author\'s name?',
-      'author.url': 'Author\'s URL?',
-      'author.username': 'Author\'s GitHub username?',
-      'author.twitter': 'Author\'s Twitter username?',
-      'author.email': 'Author\'s email address?',
-    },
-    set: {
-      'project.name': {
-        message: 'Project name?',
-        default: name,
-        force: options.init === true
-      },
-      'project.description': {
-        message: 'Project description?',
-        force: options.init === true
-      }
+    if (typeof app.questions === 'undefined') {
+      throw new Error('expected the base-questions plugin to be registered');
     }
+    if (typeof app.pkg === 'undefined') {
+      app.use(require('base-pkg')());
+    }
+
+    var opts = merge({}, config, this.options);
+    app.cache.data = app.cache.data || {};
+
+    function store(prop) {
+      if (app.globals && app.globals.get) {
+        return app.globals.get(prop);
+      }
+      return app.pkg.get(prop);
+    }
+
+    /**
+     * Author questions
+     */
+
+    app.questions.disable('save')
+      .set('author.name', 'Author\'s name?', {
+        default: store('author.name')
+      })
+      .set('author.username', 'Author\'s GitHub username?', {
+        default: store('author.username')
+      })
+      .set('author.twitter', 'Author\'s twitter username?', {
+        default: store('author.twitter')
+      })
+      .set('author.email', 'Author\'s email address?', {
+        default: store('author.email')
+      })
+      .set('author.url', 'Author\'s URL?', {
+        default: store('author.url')
+      });
+
+    /**
+     * Project questions
+     */
+
+    app.questions.disable('save')
+      .set('name', 'Project name?', {
+        default: projectName(app)
+      })
+      .set('alias', 'Project alias?', {
+        default: projectAlias(app)
+      })
+      .set('description', 'Project description?', {
+        default: app.pkg.get('description')
+      })
+      .set('version', 'Project version?', {
+        default: app.pkg.get('version') || '0.1.0'
+      })
+      .set('owner', 'Project owner?', {
+        default: projectOwner(app)
+      })
   };
 };
+
+function projectName(app) {
+  return app.cache.data.name || app.pkg.get('name') || app.project;
+}
+
+function projectAlias(app) {
+  if (typeof app.aliasFn === 'function') {
+    return app.aliasFn.call(app, projectName(app));
+  }
+  if (typeof app.options.aliasFn === 'function') {
+    return app.options.aliasFn.call(app, projectName(app));
+  }
+  var name = projectName(app);
+  if (typeof name === 'string') {
+    return name.slice(name.lastIndexOf('-') + 1);
+  }
+}
+
+function projectOwner(app) {
+  if (!userIsOwner(app)) {
+    return app.cache.data.owner;
+  }
+  var name = app.cache.data.name;
+  if (/^base-/.test(name)) {
+    return 'node-base';
+  }
+  if (/^assemble-/.test(name)) {
+    return 'assemble';
+  }
+  if (/^generate-/.test(name)) {
+    return 'generate';
+  }
+  if (/^(helper|handlebars-helper)-/.test(name)) {
+    return 'helpers';
+  }
+  if (/^updater?-/.test(name)) {
+    return 'update';
+  }
+  if (/^verb-/.test(name)) {
+    return 'verbose';
+  }
+  return app.cache.data.owner;
+}
+
+function userIsOwner(app) {
+  var author = app.cache.data.author;
+  if (typeof author === 'undefined') {
+    return true;
+  }
+  if (author && typeof author === 'object') {
+    author = author.url || author.username;
+  }
+  var git = require('parse-git-config');
+  var config = git.sync({cwd: app.cwd});
+  if (!config || !config.user) {
+    config = git.sync('global');
+  }
+  if (!config || !config.user || typeof config.user.name !== 'string') {
+    return;
+  }
+  var re = new RegExp(config.user.name, 'i');
+  return re.test(author);
+}
--- test.js
+++ test.js
@@ -2,14 +2,31 @@
 
 require('mocha');
 var assert = require('assert');
-var commonQuestions = require('./');
+var Base = require('base');
+var ask = require('base-questions');
+var pkg = require('base-pkg');
+var questions = require('./');
+var base;
 
 describe('common-questions', function() {
-  it('should export a function', function() {
-    assert.equal(typeof commonQuestions, 'function');
+  beforeEach(function() {
+    base = new Base();
+    base.isApp = true;
+    base.use(ask());
+    base.use(pkg());
   });
 
-  it('should return an object of questions', function() {
-    assert.equal(typeof commonQuestions(), 'object');
+  it('should export a function', function() {
+    assert.equal(typeof questions, 'function');
+  });
+
+  it('should work as a plugin', function() {
+    base.use(questions());
+    assert(base.questions.cache.hasOwnProperty('name'));
+    assert(base.questions.cache.hasOwnProperty('alias'));
+    assert(base.questions.cache.hasOwnProperty('owner'));
+    assert(base.questions.cache.hasOwnProperty('description'));
+    assert(base.questions.cache.hasOwnProperty('author.name'));
+    assert(base.questions.cache.hasOwnProperty('author.url'));
   });
 });
