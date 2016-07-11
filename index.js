2ee3dace1bb1943b5a94ae174ce4f6f458b1b98a
jonschlinkert <jon.schlinkert@sellside.com>
2016-07-11 13:28:38 -0400

    refactor

--- index.js
+++ index.js
@@ -7,174 +7,49 @@
 
 'use strict';
 
-var utils = require('./utils');
+var questions = require('./lib/questions');
+var listener = require('./lib/listener');
+var utils = require('./lib/utils');
+var hints = require('./lib/hints');
 
 module.exports = function(config) {
   config = config || {};
 
-  return function(app, base) {
+  return function(app) {
     if (!utils.isValid(app, 'common-questions')) return;
+    var opts = utils.extend({}, app.options, config);
+    app.cache.data = app.cache.data || {};
 
     /**
      * Plugins
      */
 
     app.use(utils.project());
-    app.use(utils.questions());
     app.use(utils.pkg());
-
-    this.cache.data = this.cache.data || {};
-    if (typeof app.option === 'function') {
-      app.option(config);
-    } else {
-      utils.merge(this.options, config);
-    }
+    app.use(utils.questions());
 
     /**
-     * Author questions
+     * Questions
+     *
+     * Default prompt messages. These can be be overridden in your app
+     * by creating a new question with the same key. For example, the
+     * folling will override the `name` question:
+     *
+     * ```js
+     * app.question('name', 'custom message...');
+     * ```
      */
 
-    app.questions.disable('save')
-      .set('author.name', 'Author\'s name?', {
-        default: store(app, 'author.name')
-      })
-      .set('author.username', 'Author\'s GitHub username?', {
-        default: store(app, 'author.username')
-      })
-      .set('author.twitter', 'Author\'s twitter username?', {
-        default: store(app, 'author.twitter')
-      })
-      .set('author.email', 'Author\'s email address?', {
-        default: store(app, 'author.email')
-      })
-      .set('author.url', 'Author\'s URL?', {
-        default: store(app, 'author.url')
-      });
+    questions.all(app, opts);
 
     /**
-     * Project questions
+     * Hints. Provides hints for the default prompts.
      */
 
-    app.questions.disable('save')
-      .set('project.name', 'Project name?', {
-        default: projectName(app)
-      })
-      .set('project.alias', 'Project alias?', {
-        default: projectAlias(app)
-      })
-      .set('project.description', 'Project description?', {
-        default: app.pkg.get('description')
-      })
-      .set('project.version', 'Project version?', {
-        default: app.pkg.get('version') || '0.1.0'
-      })
-      .set('project.owner', 'Project owner?', {
-        default: projectOwner(app)
-      });
-
-    app.questions.disable('save')
-      .set('name', 'Project name?', {
-        default: projectName(app)
-      })
-      .set('alias', 'Project alias?', {
-        default: projectAlias(app)
-      })
-      .set('description', 'Project description?', {
-        default: app.pkg.get('description')
-      })
-      .set('version', 'Project version?', {
-        default: app.pkg.get('version') || '0.1.0'
-      })
-      .set('owner', 'Project owner?', {
-        default: projectOwner(app)
-      });
+    listener(app, opts);
   };
 };
 
-function store(app, prop) {
-  var expanded = app.pkg.expand();
-  utils.merge(app.cache.data, expanded);
-  var val = app.get(['cache.data', prop]);
-  if (typeof val !== 'undefined') {
-    return val;
-  }
-  if (app.store && typeof app.store.has === 'function' && app.store.has(prop)) {
-    return app.store.get(prop);
-  }
-  if (app.globals && typeof app.globals.has === 'function' && app.globals.has(prop)) {
-    return app.globals.get(prop);
-  }
-}
-
-function getAuthor(app) {
-  var author = app.cache.data.author;
-  if (!utils.isObject(author)) {
-    var expanded = app.pkg.expand();
-    var val = expanded.get('author');
-    if (utils.isObject(val)) {
-      author = val;
-    }
-  }
-  return author;
-}
-
-function projectName(app) {
-  return app.get('cache.data.name')
-    || app.get('cache.data.project.name')
-    || app.pkg.get('name')
-    || app.project;
-}
-
-function projectAlias(app) {
-  if (typeof app.toAlias === 'function') {
-    return app.toAlias.call(app, projectName(app));
-  }
-  if (typeof app.options.toAlias === 'function') {
-    return app.options.toAlias.call(app, projectName(app));
-  }
-  var name = projectName(app);
-  if (typeof name === 'string') {
-    return name.slice(name.lastIndexOf('-') + 1);
-  }
-}
-
-function projectOwner(app) {
-  if (!userIsOwner(app)) {
-    return app.cache.data.owner;
-  }
-  var name = app.cache.data.name;
-  if (/^base-/.test(name)) {
-    return 'node-base';
-  }
-  if (/^assemble-/.test(name)) {
-    return 'assemble';
-  }
-  if (/^generate-/.test(name)) {
-    return 'generate';
-  }
-  if (/^(helper|handlebars-helper)-/.test(name)) {
-    return 'helpers';
-  }
-  if (/^updater?-/.test(name)) {
-    return 'update';
-  }
-  if (/^verb-/.test(name)) {
-    return 'verbose';
-  }
-  return app.get('cache.data.owner') || app.get('cache.data.author.username');
-}
-
-function userIsOwner(app) {
-  var username = app.get('cache.data.author.username');
-  if (typeof username === 'undefined') {
-    return true;
-  }
-  var config = utils.parse.sync({cwd: app.cwd});
-  if (!config || !config.user) {
-    config = utils.parse.sync('global');
-  }
-  if (!config || !config.user || typeof config.user.name !== 'string') {
-    return;
-  }
-  return username.indexOf(config.user.name) !== -1;
-}
+module.exports.disableHints = hints;
+module.exports.questions = questions;
+module.exports.listener = listener;
--- test.js
+++ test.js
@@ -3,40 +3,99 @@
 require('mocha');
 var assert = require('assert');
 var Base = require('base');
-var ask = require('base-questions');
-var pkg = require('base-pkg');
+var extend = require('extend-shallow');
+var askWhen = require('ask-when');
+var hints = require('./lib/hints');
 var questions = require('./');
-var base;
+var pkg = require('./package');
+var app;
 
 describe('common-questions', function() {
   beforeEach(function() {
-    base = new Base();
-    base.isApp = true;
-    base.use(ask());
-    base.use(pkg());
+    app = new Base({isApp: true});
+    app.use(questions());
+    app.use(askWhen());
   });
 
-  it('should export a function', function() {
-    assert.equal(typeof questions, 'function');
+  describe('plugin', function() {
+    it('should export a function', function() {
+      assert.equal(typeof questions, 'function');
+    });
+
+    it('should work as a plugin', function(cb) {
+      var opts = {name: 'foo', askWhen: 'not-answered'};
+      app.askWhen('name', opts, function(err, answers) {
+        if (err) return cb(err);
+
+        assert.equal(answers.name, 'foo');
+        assert(app.questions.cache.hasOwnProperty('project.name'));
+        assert(app.questions.cache.hasOwnProperty('project.alias'));
+        assert(app.questions.cache.hasOwnProperty('project.owner'));
+        assert(app.questions.cache.hasOwnProperty('project.description'));
+        assert(app.questions.cache.hasOwnProperty('author.name'));
+        assert(app.questions.cache.hasOwnProperty('author.url'));
+        cb();
+      });
+    });
   });
 
-  it('should work as a plugin', function() {
-    base.use(questions());
-    assert(base.questions.cache.hasOwnProperty('project.name'));
-    assert(base.questions.cache.hasOwnProperty('project.alias'));
-    assert(base.questions.cache.hasOwnProperty('project.owner'));
-    assert(base.questions.cache.hasOwnProperty('project.description'));
-    assert(base.questions.cache.hasOwnProperty('author.name'));
-    assert(base.questions.cache.hasOwnProperty('author.url'));
-  });
+  describe('hints', function() {
+    it('should add a hint to the `name` question', function() {
+      var question = app.questions.get('name');
+      assert.equal(question.default, pkg.name);
+    });
 
-  it('should work as a plugin', function() {
-    base.use(questions());
-    assert(base.questions.cache.hasOwnProperty('project.name'));
-    assert(base.questions.cache.hasOwnProperty('project.alias'));
-    assert(base.questions.cache.hasOwnProperty('project.owner'));
-    assert(base.questions.cache.hasOwnProperty('project.description'));
-    assert(base.questions.cache.hasOwnProperty('author.name'));
-    assert(base.questions.cache.hasOwnProperty('author.url'));
+    it('should add a hint to the `alias` question', function() {
+      var question = app.questions.get('alias');
+      assert.equal(question.default, 'commonQuestions');
+    });
+
+    it('should add a hint to the `owner` question', function() {
+      var question = app.questions.get('owner');
+      assert.equal(question.default, 'generate');
+    });
+
+    it('should add a hint to the `owner` question, using app.cache.data', function() {
+      app.registered = {};
+      app.set('cache.data.owner', 'foo');
+      app.use(questions());
+
+      var question = app.questions.get('owner');
+      assert.equal(question.default, 'foo');
+    });
+
+    it('should add a hint to the `description` question', function() {
+      var question = app.questions.get('description');
+      assert.equal(question.default, pkg.description);
+    });
+
+    it('should add a hint to the `version` question', function() {
+      var question = app.questions.get('version');
+      assert.equal(question.default, pkg.version);
+    });
+
+    it('should add a hint to the `license` question', function() {
+      var question = app.questions.get('license');
+      assert.equal(question.default, 'MIT');
+    });
+
+    it('should add a hint for author.name', function() {
+      var question = app.questions.get('author.name');
+      assert.equal(question.default, 'Jon Schlinkert');
+    });
+
+    it('should add a hint for author.name', function() {
+      var question = app.questions.get('author.name');
+      assert.equal(question.default, 'Jon Schlinkert');
+    });
+
+    it('should get author.name from app.cache.data', function() {
+      app.registered = {};
+      app.set('cache.data.author.name', 'Brian Woodward');
+      app.use(questions());
+
+      var question = app.questions.get('author.name');
+      assert.equal(question.default, 'Brian Woodward');
+    });
   });
 });
